2e53f8164cc7818801cb4ed0ccb825932bc1ec0c
Alex Witko <aleksander.witko@gmail.com>
2021-08-30 14:54:43 +0200

    After merge fix
    .

--- lapka/lib/screens/my_pets/my_pets_page.dart
+++ lapka/lib/screens/my_pets/my_pets_page.dart
@@ -4,7 +4,7 @@ import 'package:lapka/components/app_bar/custom_app_bar.dart';
 import 'package:lapka/components/basic/basic_button.dart';
 import 'package:lapka/components/basic/basic_text.dart';
 import 'package:lapka/components/basic/loading_indicator.dart';
-import 'package:lapka/components/screens/myPets/myPetComp.dart';
+import 'package:lapka/components/screens/my_pets/my_pet_comp.dart';
 import 'package:lapka/models/pet.dart';
 import 'package:lapka/providers/myPetsProvider.dart';
 import 'package:lapka/providers/my_pets/bloc/my_pets_bloc.dart';
